6f81a2f4bab4c445241bfb5b1abf16f411f483e8
Sergey Georgiev <sgrgv_art@mail.ru>
2023-08-28 20:42:26 +0300

    create app and app.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useAppDispatch } from "./hook";
 
-function App() {
+//reducers
+import { addTodo } from "./store/todoSlice";
+
+//components
+import NewTodoForm from "./components/NewTodoForm";
+import TodoList from "./components/TodoList";
+
+import "./App.css";
+
+const App: React.FC = () => {
+  const [text, setText] = useState("");
+  const dispatch = useAppDispatch();
+
+  const handleAction = () => {
+    if (text.trim().length) {
+      dispatch(addTodo(text));
+      setText("");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewTodoForm
+        value={text}
+        updateText={setText}
+        handleAction={handleAction}
+      />
+      <TodoList />
     </div>
   );
-}
+};
 
 export default App;
